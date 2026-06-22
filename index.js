31a189738d564ebab395c22250de7dc89b76019d
danieljoucoski <daniel.joucoski@escola.pr.gov.br>
2026-06-22 12:41:05 -0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require("express");
 const fs = require("fs");
-const cors = require("cors");
+const cors = require("cors")
+const path = require("path")
+
+
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use(cors());
 
-//http://localhost:3000/saudacao?nome=maria
+const clientesFile = path.join(__dirname, "clientes.json");
+
+function salvarClientes(clientes) {
+    fs.writeFileSync(clientesFile, JSON.stringify(clientes, null, 2), "utf-8")
+}
+
+    function lerClientes(){
+        if(!fs.existsSync(clientesFile)){
+            return [];
+        }
+        const dados=fs.readFileSync(clientesFile, "utf-8")
+        try {
+            return JSON.parse(dados) || [];
+        }
+        catch(e){
+            return []
+        }
+    }
+    
+
+
+app.post("/clientes", (req, res) => {
+    const {nome, cpf, cep, rua, cidade, estado, numero} = req.body
+    if(!nome || !cpf || !cep){
+        return res.status(404).json({erro: "dados incompletos"})
+    }
+    const clientes= lerClientes();
+    if(clientes.some(c => c.cpf===cpf )){
+        return res.status(400).json({erro: "cliente já cadastrado"})
+    }
+    const novoCliente={nome, cpf, cep, rua, cidade, estado, numero};
+    clientes.push(novoCliente);
+    salvarClientes(clientes);
+     return res.status(201).json({mensagem: "cliente cadastrado com sucesso"})
+
+})
+
+//http://localhost:3000/saudacao?nome=rafa
 app.get("/saudacao", (req, res) => {
     const nome = req.query.nome;
     if (!nome) {
@@ -55,7 +96,7 @@ app.post("/login", (req, res) => {
     if (!email || !senha) {
         return res.status(404).json({ erro: "dados incompletos" })
     }
-     if (email === "admin@admin.com" && senha === "123456") {
+    if (email === "admin@admin.com" && senha === "123456") {
         return res.status(200).json({ token: "123456" })
     } else {
         return res.status(404).json({ erro: "senha ou email incorreto" })
